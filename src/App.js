96ee3f2b3cfaa24b0c8dfb5bc2980f5dd73c53bc
asyrafz <asyrafzain96@gmail.com>
2022-12-24 19:55:50 +0800

    Signed-off-by: asyraf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/sidebar';
 import Header from './components/Header';
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import MenuIcon from '@mui/icons-material/Menu';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import {RxHamburgerMenu} from 'react-icons/rx';
+import {GrHomeRounded} from 'react-icons/gr';
+import {SiPlaycanvas} from 'react-icons/si';
+import {BsCollectionPlay} from 'react-icons/bs';
+import {MdVideoLibrary} from 'react-icons/md';
 
 
 function Sidebar() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <MenuIcon style={{ width: '40px', height: '40px' }} />
-        <HomeOutlinedIcon style={{ width: '40px', height: '40px' }} />
+    <div style={{ display: 'flex', flexDirection: 'column', padding: '0 10px'}}>
+        
+
+        <RxHamburgerMenu style={{ width: '25px', height: '25px', padding: '10px 0'}}/>
+        <GrHomeRounded style={{ width: '25px', height: '25px', padding: '10px 0'}}/>
+        <SiPlaycanvas style={{ width: '25px', height: '25px', padding: '10px 0'}}/>
+        <BsCollectionPlay style={{ width: '25px', height: '25px', padding: '10px 0'}}/>
+        <MdVideoLibrary style={{ width: '25px', height: '25px', padding: '10px 0'}}/>
+        
+
     </div>
   )
 }
